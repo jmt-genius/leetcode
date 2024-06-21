bc446b86c7ae25e0424d72482b774d53fc0b1af5
J M Tarun <82860323+jmt-genius@users.noreply.github.com>
2024-06-22 00:02:36 +0530

    Time: 393 ms (5.01%) | Memory: 34.3 MB (82.09%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int initialSatisfaction = 0;
-        int maxExtraSatisfaction = 0;
-        int currentWindowSatisfaction = 0;
-        
-        for (int i = 0; i < customers.size(); ++i) {
-            if (grumpy[i] == 0) {
-                initialSatisfaction += customers[i];
-            } else if (i < minutes) {
-                currentWindowSatisfaction += customers[i];
+        int ini=0;
+
+        for(int i=0;i<customers.size();i++){
+            if(grumpy[i]==0){
+                ini=ini+customers[i];
             }
         }
-        
-        maxExtraSatisfaction = currentWindowSatisfaction;
-        
-        for (int i = minutes; i < customers.size(); ++i) {
-            currentWindowSatisfaction += customers[i] * grumpy[i];
-            currentWindowSatisfaction -= customers[i - minutes] * grumpy[i - minutes];
-            maxExtraSatisfaction = max(maxExtraSatisfaction, currentWindowSatisfaction);
+        int max=0;
+        for(int i=0;i<customers.size()+1-minutes;i++){
+            int p=0;
+            for(int k=0;k<minutes;k++){
+                if(grumpy[i+k]==1){
+                    p+=customers[i+k];
+                }
+
+            }
+            if(p>max){
+                max=p;
+            }
+
         }
-        
-        return initialSatisfaction + maxExtraSatisfaction;   
+        return ini+max;
     }
 };
