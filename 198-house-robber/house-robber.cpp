6e643db79c0e2d11229949d057399f4a4aa7b494
J M Tarun <82860323+jmt-genius@users.noreply.github.com>
2025-06-06 01:56:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (22.29%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -15,8 +15,14 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n,-1);
-        int r=f(n-1,nums,dp);
         
-        return r;
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1) take+=dp[i-2];
+            int nottake=0+dp[i-1];
+            dp[i]=max(take,nottake); 
+        }
+        return dp[n-1];
     }
 };
