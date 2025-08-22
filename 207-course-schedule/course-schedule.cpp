66fd3f683864b0a3b868db64dfff35fb0592b57e
J M Tarun <82860323+jmt-genius@users.noreply.github.com>
2025-08-22 21:21:12 +0530

    Time: 6 ms (54.29%) | Memory: 20 MB (48.53%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -15,17 +15,31 @@ public:
         return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> visited(numCourses,0);
-        vector<int> dfsv(numCourses,0);
+        
         vector<vector<int>> adj(numCourses);
+        vector<int> indeg(numCourses,0);
         for(auto it:prerequisites){
             adj[it[1]].push_back(it[0]);
+            indeg[it[0]]++;
         }
+        queue<int> q;
+        int cnt=0;
         for(int i=0;i<numCourses;i++){
-            if(!visited[i]){
-                if(checkcycle(i,adj,visited,dfsv)) return false;
+            if(indeg[i]==0){
+                q.push(i);
+            }
+
+        }
+        while(!q.empty()){
+            int top=q.front();
+            q.pop();
+            cnt++;
+            for(auto it:adj[top]){
+                indeg[it]--;
+                if(indeg[it]==0) q.push(it);
             }
         }
-        return true;
+        if(cnt==numCourses) return true;
+        return false;
     }
 };
