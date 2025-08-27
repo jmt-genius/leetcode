2274d2e409a96f66a9bf55146da03e85a6d23e40
J M Tarun <82860323+jmt-genius@users.noreply.github.com>
2025-08-27 20:06:32 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (5.65%) - LeetSync

--- 2384-root-equals-sum-of-children/root-equals-sum-of-children.cpp
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.cpp
@@ -12,23 +12,7 @@
 class Solution {
 public:
     bool checkTree(TreeNode* root) {
-        if(!root) return true;
-        queue<TreeNode*> q;
-        q.push(root);
-        int sum=root->val;
-        while(!q.empty()){
-            int s=q.size();
-            int cum=0;
-            for(int i=0;i<s;i++){
-                TreeNode* node=q.front();
-                q.pop();
-                cum+=node->val;
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-            if(cum!=sum) return false;
-        }
-        return true;
+        return root->val == root->left->val + root->right->val;
         
     }
 };
