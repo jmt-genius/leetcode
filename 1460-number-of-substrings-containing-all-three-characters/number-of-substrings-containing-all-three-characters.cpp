327532baa1f1717c693d31edcecee637c3d3ac5e
J M Tarun <82860323+jmt-genius@users.noreply.github.com>
2026-07-09 01:55:47 +0530

    Time: 15 ms (28.03%) | Memory: 11.1 MB (28.26%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         vector<int> mpp(3,-1);
-        int l=0,r=0,sum=0;
+        int r=0,sum=0;
         while(r<s.size()){
             mpp[(int)s[r]-'a']=r;
             int mini=*min_element(mpp.begin(),mpp.end());
