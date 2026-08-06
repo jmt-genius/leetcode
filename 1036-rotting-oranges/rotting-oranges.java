cecd321a3c49236d6686f133add204eadf1fc993
J M Tarun <82860323+jmt-genius@users.noreply.github.com>
2026-08-06 20:07:08 +0530

    Time: 2 ms (89.95%) | Memory: 44.4 MB (25.04%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        int[][] vis = new int[n][m];
+
+        // Add all initially rotten oranges to the queue
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j, 0}); // row, col, time
+                    vis[i][j] = 2;
+                } else {
+                    vis[i][j] = 0;
+                }
+            }
+        }
+
+        int[] dRow = {-1, 0, 1, 0};
+        int[] dCol = {0, 1, 0, -1};
+
+        int time = 0;
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int row = curr[0];
+            int col = curr[1];
+            int t = curr[2];
+
+            time = Math.max(time, t);
+
+            for (int i = 0; i < 4; i++) {
+                int newRow = row + dRow[i];
+                int newCol = col + dCol[i];
+
+                if (newRow >= 0 && newRow < n &&
+                    newCol >= 0 && newCol < m &&
+                    vis[newRow][newCol] == 0 &&
+                    grid[newRow][newCol] == 1) {
+
+                    queue.offer(new int[]{newRow, newCol, t + 1});
+                    vis[newRow][newCol] = 2;
+                }
+            }
+        }
+
+        // Check if any fresh orange remains
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1 && vis[i][j] != 2) {
+                    return -1;
+                }
+            }
+        }
+
+        return time;
+    }
+}
